590ccabcfaaa87ea9ddbb170ff8e6c116f66795f
wangchupei <972904383@qq.com>
2020-10-28 17:55:20 +0800

    中文注释mybatis

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,55 @@
+/**
+ *    Copyright ${license.git.copyrightYears} the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package test;
+
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.SystemMetaObject;
+import org.apache.ibatis.reflection.TypeParameterResolver;
+import org.apache.ibatis.reflection.property.PropertyNamer;
+import sun.reflect.generics.reflectiveObjects.ParameterizedTypeImpl;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Type;
+import java.util.Map;
+
+public class Test {
+    protected Map map;
+
+    public Map getMap() {
+        return map;
+    }
+
+    public void setMap(Map map) {
+        this.map = map;
+    }
+
+    public static void main(String[] args) throws NoSuchFieldException {
+
+        System.out.println(PropertyNamer.methodToProperty("getAbc"));
+        System.out.println(PropertyNamer.methodToProperty("get$AA"));
+        System.out.println(PropertyNamer.methodToProperty("isA"));
+
+        Field f = Test.class.getDeclaredField("map");
+
+        System.out.println(f.getGenericType().getClass());
+        ;
+
+        Type type = TypeParameterResolver.resolveFieldType(f,ParameterizedTypeImpl
+                .make(Test.class,new Type[]{Long.class},Test.class));
+        System.out.println(type.getClass());
+
+    }
+}
